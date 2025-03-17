9aaf4cfa546a67e0e5266fdd1e27a8fc7c60f5e3
Heramb Bansod <63042748+HerambBansod@users.noreply.github.com>
2025-03-18 02:49:03 +0530

    Update firebaseConfig.js

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,5 +1,4 @@
-
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 
 const firebaseConfig = {
     apiKey: "AIzaSyC0QYvUvSdu06_8qjbERtTY03D0CvqRVU4",
@@ -8,10 +7,15 @@ const firebaseConfig = {
     storageBucket: "financetracker-a68e8.appspot.com",
     messagingSenderId: "1010540093690",
     appId: "1:1010540093690:web:a7acc5868e34ffbf57e943",
-    measurementId: "G-55F8M7YY8F", // Optional
+    measurementId: "G-55F8M7YY8F",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase only if it hasn't been initialized already
+let app;
+if (!getApps().length) {
+    app = initializeApp(firebaseConfig);
+} else {
+    app = getApp();
+}
 
-export { app }; 
+export { app };
